a2e22806d9c7b25c4fa7415cc3a2e4db0866cbd6
Wallace Oliveira <wallacedeoliveira10@hotmail.com>
2024-02-27 11:41:50 -0300

    Styles Adjustment

--- astrobooks/src/components/Header.js
+++ astrobooks/src/components/Header.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaBars, FaEnvelope, FaUser } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaBars, FaEnvelope, FaUser, FaSignOutAlt } from 'react-icons/fa';
+import { logoutUser } from './Auth'; 
 import SearchBar from './SearchBar';
 import '../styles/Header.css';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      console.log("Logout realizado com sucesso.");
+      navigate('/login');
+    } catch (error) {
+      console.error("Erro ao fazer logout:", error.message);
+    }
+  };
+
   return (
     <header>
-      <div className="header-container">
+      <div className={`header-container ${isMenuOpen ? 'menu-open' : ''}`}>
         <div className="left-section">
           <button className="menu-button" onClick={handleMenuToggle} aria-expanded={isMenuOpen}>
             <FaBars />
@@ -29,6 +41,9 @@ const Header = () => {
             <Link to="/profile">
               <FaUser title="Perfil" />
             </Link>
+            <button onClick={handleLogout} className="logout-button" title="Logout">
+              <FaSignOutAlt />
+            </button>
           </nav>
         </div>
       </div>
